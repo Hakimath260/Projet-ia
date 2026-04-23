66f2d29e6928ae7018366f51eb215aad1b5d1a44
Hakimath260 <Hakim.Ait-Hamou@etu.univ-smb.fr>
2026-04-23 09:34:05 +0200

    Update 01_download_meteostat.py

--- ml/scripts/01_download_meteostat.py
+++ ml/scripts/01_download_meteostat.py
@@ -1,1 +1,21 @@
+pip install meteostat pandas numpy scikit-learn matplotlib tensorflow tf2onnx onnx
+from meteostat import Point, Hourly
+from datetime import datetime
+import pandas as pd
+from pathlib import Path
 
+out_dir = Path("data/raw")
+out_dir.mkdir(parents=True, exist_ok=True)
+
+# Exemple : Chambéry
+location = Point(45.5646, 5.9178)
+
+start = datetime(2022, 1, 1)
+end = datetime(2024, 12, 31)
+
+df = Hourly(location, start, end).fetch()
+df.to_csv(out_dir / "meteostat_chambery_hourly.csv")
+
+print(df.head())
+print(df.columns.tolist())
+print(df.shape)
